de62815532f591cebf8be8a149e7bd4daed95601
Vinícius Meneses <viniciusmenesesdev@gmail.com>
2024-04-17 12:33:59 -0300

    first commit

---
+++ thymeleaftutorial/src/main/java/com/javaguides/thymeleaftutorial/ThymeleaftutorialApplication.java
@@ -0,0 +1,13 @@
+package com.javaguides.thymeleaftutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleaftutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleaftutorialApplication.class, args);
+	}
+
+}
---
+++ thymeleaftutorial/src/test/java/com/javaguides/thymeleaftutorial/ThymeleaftutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaguides.thymeleaftutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleaftutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
